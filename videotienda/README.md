09df299e1210f992e1429f7bb8370624373c76bb
Cristhian Medina <cristhian.medina.or@hotmail.com>
2022-09-26 00:59:33 -0500

    26/09/2022

---
+++ videotienda/README.md
@@ -0,0 +1,8 @@
+# Proyecto de Videotienda
+Este proyecto es el logro realizado en elciclo 3 de mision tic 2022 UTP
+
+**Autor**: Cristhian Medina
+
+
+
+
---
+++ videotienda/src/main/java/co/edu/utp/misiontic/cesardiaz/videotienda/controller/VideoShopController.java
@@ -0,0 +1,71 @@
+package co.edu.utp.misiontic.cesardiaz.videotienda.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import co.edu.utp.misiontic.cesardiaz.videotienda.service.CatalogService;
+import lombok.extern.slf4j.Slf4j;
+
+// Spring MVC
+
+@Controller
+@Slf4j
+public class VideoShopController {
+
+    private CatalogService catalogService;
+
+    public VideoShopController(CatalogService catalogService) {
+        this.catalogService = catalogService;
+    }
+
+    @GetMapping("/catalog")
+    public String goToCatalog(Model model) {
+
+        var categories = this.catalogService.getCategory();
+
+        model.addAttribute("title", "Welcome to my site");
+        model.addAttribute("categories", categories);
+
+        return "catalog";
+    }
+
+    @GetMapping("/catalog/{id}")
+    public String loadCatalogById(@PathVariable("id") Integer id, Model model) {
+        log.info("Cargando informacion de categoria {}", id);
+
+        var categories = catalogService.getCategory();
+        model.addAttribute("categories", categories);
+
+        var categoryOp = this.catalogService.getCategoryById(id);
+        
+        if (categoryOp.isEmpty()) {
+            // Mostrar mensaje de error
+            model.addAttribute("error", "La categoria no existe");
+        } else {
+
+            var category = categoryOp.get();
+
+            model.addAttribute("title", category.getName());
+            model.addAttribute("id", category.getId());
+
+            var categoryMovies = this.catalogService.getMoviesByCategoryId(id);
+
+            model.addAttribute("movies", categoryMovies);
+        }
+
+        return "catalog";
+    }
+
+    @GetMapping("/contact")
+    public String goToContact(Model model) {
+        return "contact";
+    }
+
+    @GetMapping("/login")
+    public String goToLogin(Model model) {
+        return "login";
+    }
+}
